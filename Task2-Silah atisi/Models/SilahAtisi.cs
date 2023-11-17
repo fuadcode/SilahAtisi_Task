2ff2a8966580221c29832ebf556b177f26c479fa
Fuad <sabirkg@code.edu.az>
2023-11-18 01:32:26 +0400

    Silah_Atışı_Task

---
+++ Task2-Silah atisi/Models/SilahAtisi.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Task2_Silah_atisi.Models
+{
+    public class SilahAtisi
+    {
+        public int GulleMiqdari = 32;
+
+        public void GulleTekAtis()
+        {
+            int Daraq = 32;
+            GulleMiqdari--;
+            Console.WriteLine($"Gulle atildigca 1 eded mermi azalir {GulleMiqdari} /{Daraq}");
+
+
+            if (GulleMiqdari == 0)
+            {
+                Console.WriteLine("Daraqdaki gulle sayi azalir..");
+                ReloadMagazine();
+                return;
+            }
+        }
+        public void ReloadMagazine()
+        {
+            if (GulleMiqdari==32)
+            {
+                Console.WriteLine("Daraq tam doludur..");
+                return;
+            }
+            GulleMiqdari = 32;
+         }
+        public void UcluAtis()
+        {
+            int Daraq = 32;
+            GulleMiqdari = GulleMiqdari - 3;
+            Console.WriteLine($"Gulle atildigca uc-uc azalir {GulleMiqdari} /{Daraq}");
+           
+            if (GulleMiqdari == 0)
+            {
+                Console.WriteLine("Daraqdaki gulle sayi azalir..");
+               ReloadMagazine();
+                return;
+            }
+        }
+        public void MetodCagirmaq()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo Metodinfo = Console.ReadKey();
+                switch (Metodinfo.Key)
+                {
+                    case ConsoleKey.D:
+                        GulleTekAtis();
+                        break;
+                    case ConsoleKey.U:
+                        UcluAtis();
+                        break;
+                    case ConsoleKey.R:
+                        ReloadMagazine();
+                        break;
+                }
+            }
+        }
+        public void SilahiCagirmaq()
+        {
+            MetodCagirmaq();
+        }
+    }
+}
---
+++ Task2-Silah atisi/Program.cs
@@ -0,0 +1,7 @@
+﻿#region task2
+using Task2_Silah_atisi.Models;
+
+SilahAtisi silahAtisi = new();
+silahAtisi.SilahiCagirmaq();
+
+#endregion
